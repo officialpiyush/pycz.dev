016966658e0f1cb981c75b69cc54bafb1ec0a9c5
Piyush <bhangalepiyush@gmail.com>
2023-03-29 19:59:26 +0530

    feat(database): add links table schema

---
+++ src/utils/db/schema.ts
@@ -0,0 +1,20 @@
+import { nanoid } from "nanoid";
+import { boolean, text, timestamp } from "drizzle-orm/mysql-core/columns";
+import { mysqlTable } from "drizzle-orm/mysql-core/table";
+import { uniqueIndex } from "drizzle-orm/mysql-core/indexes";
+
+export const links = mysqlTable(
+  "links",
+  {
+    id: text("id").primaryKey().default(nanoid()),
+    key: text("key"),
+    parent: text("parent"),
+    description: text("description"),
+    url: text("url"),
+    createdAt: timestamp("created_at", { fsp: 2 }).notNull().defaultNow(),
+    enabled: boolean("enabled").default(true),
+  },
+  (links) => ({
+    keyIndex: uniqueIndex("key_idx").on(links.key),
+  })
+);
