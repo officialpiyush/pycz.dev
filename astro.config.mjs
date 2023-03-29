effe5624fa1df288f0ed7449b1961a93af189914
Piyush <bhangalepiyush@gmail.com>
2023-03-29 20:39:57 +0530

    feat(ssr): add vercel edge adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,11 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
+import vercel from "@astrojs/vercel/edge";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
   integrations: [tailwind()],
+  adapter: vercel()
 });
