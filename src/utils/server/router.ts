cf4be0d32ef11edabaffb63e15a23276d600dbc9
Piyush <bhangalepiyush@gmail.com>
2023-03-29 23:22:59 +0530

    feat(trpc): add middleware to protect route to add links

--- src/utils/server/router.ts
+++ src/utils/server/router.ts
@@ -1,14 +1,27 @@
-import { initTRPC } from "@trpc/server";
+import { initTRPC, TRPCError } from "@trpc/server";
 import { z } from "zod";
 import type { Context } from "./context";
 
 export const t = initTRPC.context<Context>().create();
 
+const isAuthed = t.middleware(({ ctx, next }) => {
+  if (!ctx.isAuthed) {
+    throw new TRPCError({
+      code: "UNAUTHORIZED",
+      message: "Missing x-dashboard-key header",
+    });
+  }
+
+  return next();
+});
+
+export const protectedRouter = t.procedure.use(isAuthed);
+
 export const appRouter = t.router({
   getLinkByKey: t.procedure.input(z.string()).query(({ input }) => {
     return `https://pycz.dev`;
   }),
-  createLink: t.procedure
+  createLink: protectedRouter
     .input(
       z.object({
         key: z.string().min(1),
