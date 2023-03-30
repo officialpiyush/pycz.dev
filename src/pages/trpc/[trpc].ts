7a3f80fd79d29b693a93be2ef391a2faff7505bc
Piyush <bhangalepiyush@gmail.com>
2023-03-30 15:44:47 +0530

    fix(trpc): catch all routes trpc

--- src/pages/trpc/[trpc].ts
+++ src/pages/trpc/[trpc].ts
@@ -1,7 +1,7 @@
 import { fetchRequestHandler } from "@trpc/server/adapters/fetch";
 import type { APIRoute } from "astro";
-import { createContext } from "../utils/server/context";
-import { appRouter } from "../utils/server/router";
+import { createContext } from "../../utils/server/context";
+import { appRouter } from "../../utils/server/router";
 
 export const all: APIRoute = ({ params, request }) => {
   return fetchRequestHandler({
