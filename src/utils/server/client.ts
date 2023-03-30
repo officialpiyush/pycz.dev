4d4942dc512d57c9cccf40a994da7a11f50c46be
Piyush <bhangalepiyush@gmail.com>
2023-03-30 16:02:39 +0530

    feat(trpc): use token from local storage

--- src/utils/server/client.ts
+++ src/utils/server/client.ts
@@ -1,10 +1,15 @@
 import { createTRPCProxyClient, httpBatchLink } from "@trpc/client";
 import type { AppRouter } from "./router";
 
+const key = localStorage.getItem("key");
+
 const client = createTRPCProxyClient<AppRouter>({
   links: [
     httpBatchLink({
       url: "/trpc",
+      headers: {
+        "x-dashboard-key": key || "",
+      },
     }),
   ],
 });
