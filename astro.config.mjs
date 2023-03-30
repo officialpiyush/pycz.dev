66f29fb93ca26e9cafbb94c933f1b198c5af421c
Piyush <bhangalepiyush@gmail.com>
2023-03-30 15:26:35 +0530

    chore: add preact config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
 import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 import vercel from "@astrojs/vercel/edge";
 
 // https://astro.build/config
+import preact from "@astrojs/preact";
+
+// https://astro.build/config
 export default defineConfig({
   output: "server",
-  integrations: [tailwind()],
+  integrations: [tailwind(), preact()],
   adapter: vercel()
 });
