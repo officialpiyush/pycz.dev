e2befac6c024d955a5a83a4b9282d6a01ad07629
Piyush <bhangalepiyush@gmail.com>
2023-05-07 11:58:29 +0530

    chore(astro): use lagon adapter instead of vercel edge

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
-import vercel from "@astrojs/vercel/edge";
+import lagon from "@lagon/astro";
 import preact from "@astrojs/preact";
 import Icons from "unplugin-icons/vite";
 
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), preact()],
-  adapter: vercel(),
+  adapter: lagon(),
   vite: {
     plugins: [
       Icons({
