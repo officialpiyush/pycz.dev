d2a55e0f53df558320a7a60bf9f2948b565b5d9c
Piyush <bhangalepiyush@gmail.com>
2023-05-07 13:10:53 +0530

    chore(CD): use environment variables to determine the provider for CD

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,16 @@
 import { defineConfig } from "astro/config";
 
-import tailwind from "@astrojs/tailwind";
-import lagon from "@lagon/astro";
 import preact from "@astrojs/preact";
+import tailwind from "@astrojs/tailwind";
 import Icons from "unplugin-icons/vite";
+
+import vercel from "@astrojs/vercel/edge";
+import lagon from "@lagon/astro";
 
 export default defineConfig({
   output: "server",
   integrations: [tailwind(), preact()],
-  adapter: lagon(),
+  adapter: process.env.VERCEL ? vercel() : lagon(),
   vite: {
     plugins: [
       Icons({
